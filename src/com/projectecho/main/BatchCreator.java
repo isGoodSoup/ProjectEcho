2c20c4430fce1357bd6c132441f76222a32bee1b
isGoodSoup <sargent.pointed453@passinbox.com>
2025-10-31 15:40:05 +0100

    feat: added "pause" statement

--- src/com/projectecho/main/BatchCreator.java
+++ src/com/projectecho/main/BatchCreator.java
@@ -14,26 +14,30 @@ import com.projectecho.utils.Utils;
 public class BatchCreator {
 	private Terminal terminal;
 	private LineReader reader;
+	private String home, file, path;
+	private boolean pause;
 	private Utils util = new Utils();
 	
 	public BatchCreator() {
 		try {
 			terminal = TerminalBuilder.builder().system(true).build();
+			reader = LineReaderBuilder.builder()
+		            .terminal(terminal)
+		            .build();
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
-		reader = LineReaderBuilder.builder()
-		            .terminal(terminal)
-		            .build();
 	}
 	
 	public void createScript() throws IOException {
-		String home = System.getenv("USERPROFILE");
-		String file = util.toScan(reader, "Insert the script name");
-		String path = home + "\\" + file + ".bat";
+		home = System.getenv("USERPROFILE");
+		file = util.toScan(reader, "Script name");
+		pause = util.toScanBoolean(reader, "Pause statement");
+		path = home + "\\" + file + ".bat";
 		try(BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
 			writer.write("@echo off");
 			writer.newLine();
+			if(pause) writer.write("pause"); writer.newLine();
 		}
 		System.out.println("Created: " + path);
 	}
--- src/com/projectecho/utils/Utils.java
+++ src/com/projectecho/utils/Utils.java
@@ -8,4 +8,10 @@ public class Utils {
 		System.out.print(prompt + ": ");
 		return reader.readLine();
 	}
+	
+	public boolean toScanBoolean(LineReader reader, String prompt) {
+	    System.out.print(prompt + ": ");
+	    String input = reader.readLine().trim().toLowerCase();
+	    return input.equals("true") || input.equals("yes") || input.equals("y");
+	}
 }
